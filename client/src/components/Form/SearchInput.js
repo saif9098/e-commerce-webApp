d4ec61552f02257b996c73dedc54075adc6a1049
Mo. Saif raja <126969644+saif9098@users.noreply.github.com>
2024-10-30 16:27:08 +0530

    Add files via upload

---
+++ client/src/components/Form/SearchInput.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useSearch } from "../../context/search";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const SearchInput = () => {
+  const [values, setValues] = useSearch();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.get(
+        `/api/v1/product/search/${values.keyword}`
+      );
+      setValues({ ...values, results: data });
+      navigate("/search");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <form className="d-flex" role="search" onSubmit={handleSubmit}>
+        <input
+          className="form-control form-control-sm me-2 pt-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          value={values.keyword}
+          onChange={(e) => setValues({ ...values, keyword: e.target.value })}
+        />
+        <button className="btn btn-sm btn-outline-success pt-2 me-3" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchInput;
